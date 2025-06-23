8e60681e1d6f64c876660c9ce9b017af44930431
Pratik Mittal <mittalpratik1109@gmail.com>
2025-06-23 11:30:21 -0500

    importing image in a variable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import logo from "src\assets\investment-calculator-logo.png";
+
 export default function Header(){
     return(
         <header id="header">
-            <img src="src/assets/investment-calculator-logo.png" />
+            <img src={logo} />
             <h1> Investment Calculator</h1>
         </header>
     );
